bed86d876bbb2e24cce93cede52601e6d920398f
Philipp Jäckel <philipp.jaeckel100@gmx.de>
2019-09-03 21:02:46 +0200

    describeBeforeEach finally working
    describeBeforeEach works now and now I have to find a good name for the method

--- ng-magic-test-bed-presentation/projects/ng-magic-test-bed/src/describe-before-each/describe-before-each.function.ts
+++ ng-magic-test-bed-presentation/projects/ng-magic-test-bed/src/describe-before-each/describe-before-each.function.ts
@@ -1,49 +1,64 @@
 
-export function describeBeforeEach(describeText: string, describeCallback) {
+export let describeBeforeEach = originalDescribeBeforeEach;
+export let describeTest = (text: string, callback: () => void) => void (0);
 
-    // let index = 0;
-    // let activeIndex = 0;
-    // const lastTest = test;
-    // test = (text: string, callback) => {
-    //     if (index === activeIndex) {
-    //         callback();
-    //     }
-    //     index++;
-    // };
-    // describeCallback();
-    // const maxIndex = index;
-    // activeIndex++;
-    // while (activeIndex<maxIndex){
-    //     describeCallback();
 
-    // }
+function originalDescribeBeforeEach(describeText: string, describeCallback) {
+  describe(describeText, () => {
+    const metas = getInnerMethodMetas(describeCallback);
+    metas.forEach(meta => {
+      it(meta.text, () => {
+        callInnerMethodAtPath(describeCallback, meta.path);
+      });
+    });
+  });
 }
 
-export let test = function () {
+function getInnerMethodMetas(describeCallback): Array<IInnerMethodMeta> {
+  const metas = [];
+  forEachInnerMethod(describeCallback, meta => {
+    metas.push(meta);
+  });
+  return metas;
+}
 
-};
+function forEachInnerMethod(describeCallback, iterator: (meta: IInnerMethodMeta) => void, path: Array<number> = [], prefix: string = '') {
+  let index = 0;
+  const lastTest = describeTest;
+  const lastDescribeBeforeEach = describeBeforeEach;
+  describeBeforeEach = (text, callback) => {
+    const innerPath = [...path, index];
+    forEachInnerMethod(callback, iterator, innerPath, prefix + ' ' + text);
+    index++;
+  };
+  describeTest = (text: string, callback) => {
+    iterator({
+      path: [...path, index], text: prefix + ' ' + text, callback
+    });
+    index++;
+  };
+  describeCallback();
+  describeTest = lastTest;
+  describeBeforeEach = lastDescribeBeforeEach;
+}
 
+interface IInnerMethodMeta {
+  text: string;
+  callback: () => void;
+  path: Array<number>;
+}
 
+function callInnerMethodAtPath(describeCallback: () => void, path: Array<number>) {
+  forEachInnerMethod(describeCallback, meta => {
+    if (pathIsEqual(meta.path, path)) {
+      meta.callback();
+    }
+  });
+}
 
-
-  // describe(describeText, () => {
-    //     let first = true;
-
-    //     //while another it//TESt
-    //     beforeEach(() => {
-    //         if (first) {
-    //             first = false;
-    //             return;
-    //         }
-    //         const blackList = ['beforeEach', 'afterEach', 'xdescribe', 'describe', 'xit', 'it'];
-    //         const originals = {};
-    //         blackList.forEach(methodName => {
-    //             originals[methodName] = window[methodName];
-    //             window[methodName] = () => void(0);
-    //         });
-    //         describeCallback();
-    //         blackList.forEach(methodName => window[methodName] = originals[methodName]);
-    //     });
-
-    //     describeCallback();
-    // });
+function pathIsEqual(path1: Array<number>, path2: Array<number>) {
+  if (path1.length !== path2.length) {
+    return false;
+  }
+  return path1.every((entry, index) => path2[index] === entry);
+}
--- ng-magic-test-bed-presentation/projects/ng-magic-test-bed/src/integration-tests/describe-before-each-use-case.spec.ts
+++ ng-magic-test-bed-presentation/projects/ng-magic-test-bed/src/integration-tests/describe-before-each-use-case.spec.ts
@@ -1,5 +1,6 @@
-import { describeBeforeEach } from '../describe-before-each/describe-before-each.function';
+import { describeBeforeEach, describeTest } from '../describe-before-each/describe-before-each.function';
 
+//magicDescribe, magicTest :)
 
 describeBeforeEach('DescribeBeforeEachTest', () => {
 
@@ -7,13 +8,34 @@ describeBeforeEach('DescribeBeforeEachTest', () => {
         count: 0
     };
 
-    // it('test 1', () => {
-    //     x.count++;
-    //     expect(x.count).toEqual(1);
+    // beforeEach(() => {
+    //     x.count--;
     // });
 
-    // it('test 2', () => {
-    //     x.count++;
-    //     expect(x.count).toEqual(1);
-    // });
+    describeTest('test 1', () => {
+        x.count++;
+        expect(x.count).toEqual(1);
+    });
+
+    describeTest('test 2', () => {
+        x.count++;
+        expect(x.count).toEqual(1);
+    });
+
+    describeBeforeEach('innerDescribe', () => {
+        const y = {
+            count: 1
+        };
+        x.count++;
+        describeTest('test 3', () => {
+            y.count++;
+            expect(x.count).toEqual(1);
+            expect(y.count).toEqual(2);
+        });
+        describeTest('test 4', () => {
+            y.count++;
+            expect(x.count).toEqual(1);
+            expect(y.count).toEqual(2);
+        });
+    });
 });
